395a8ee6c5660bbe4d671d7577bb78eb62e43a4f
WenjieOoo <41363940+WenjieOoo@users.noreply.github.com>
2020-10-15 18:25:10 +0800

    一些更新
    减少了fun()的次数，更改了localbest，还有其它一些修改

--- CCPSO2.py
+++ CCPSO2.py
@@ -3,10 +3,11 @@ import numpy as np
 from scipy.stats import cauchy, norm
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class CCPSO2(object):
-    def __init__(self, fun, dimension_size, x_init_scale):
+    def __init__(self, fun, dimension_size, population_size, max_number_of_fitness_evaluations, x_lower, x_upper):
 
         # 优化的问题
         self.fun = fun
@@ -15,9 +16,10 @@ class CCPSO2(object):
 
         # 子种群大小可选集
         # self.group_size_set = [2, 5, 50, 100, 200]
-        self.group_size_set = [2, 5, 10]
+        # self.group_size_set = [2, 5, 10]
+        self.group_size_set = [2, 5, 10, 50, 100, 250]
 
-        # 子种群大小
+        # 子种群维度size
         self.s = random.choice(self.group_size_set)
         # 子种群数量
         self.k = int(self.dimension_size / self.s)
@@ -26,17 +28,20 @@ class CCPSO2(object):
         self.dimension_indices = list(range(self.dimension_size))
 
         # 粒子数量
-        self.population_size = 30
-
-        # 迭代轮数
-        self.iterations = 100
-        # self.iterations = 625 * self.dimension_size
+        self.population_size = population_size
 
         # 粒子更新方式的选择
         self.p = 0.5
 
+        # 上下界
+        self.x_lower = x_lower
+        self.x_upper = x_upper
+
         # 粒子位置
-        self.X = np.random.rand(self.population_size, self.dimension_size) * x_init_scale
+        # self.X = np.random.rand(self.population_size, self.dimension_size) * x_init_scale
+        self.X = (self.x_lower + np.random.rand(self.population_size, self.dimension_size) * (
+                self.x_upper - self.x_lower)).astype(np.float64)
+
         # 粒子个人最好位置
         self.Y = self.X.copy()
         # 粒子相邻最好位置
@@ -44,12 +49,28 @@ class CCPSO2(object):
         # 种群各自最优位置
         self.Y_swarm = self.X[0, :].copy()
 
+        # 适应值存储
+        self.fX = np.ones([self.population_size, self.k]) * np.inf
+        self.fY = self.fX.copy()
+        self.fY_swarm = np.inf
+        self.fY_global = np.inf
+
         # 全局最优位置
         self.Y_global = self.X[0, :].copy()
         # 在某一轮进化中全局最优位置是否发生过改变
         self.global_improved = False
         # 历史全局最优适应值
         self.Y_global_history = []
+
+        # 进行了多少次适应度评估
+        self.number_of_fitness_evaluations = 0
+
+        # 在第几次进化达到最大评估次数而终止
+        self.end = 0
+
+        # 最大适应度评估次数
+        self.max_number_of_fitness_evaluations = max_number_of_fitness_evaluations
+        self.pbar = tqdm(range(self.max_number_of_fitness_evaluations), desc="fitness_evaluations：", position=0)
 
     def b(self, j, i, position_type):
         particle_vector = self.Y_swarm.copy()
@@ -66,24 +87,66 @@ class CCPSO2(object):
             print("function b error.")
         return particle_vector
 
+    def b_x(self, j, i):
+        particle_vector = self.Y_swarm.copy()
+        for d in range(j * self.s, (j + 1) * self.s):
+            particle_vector[self.dimension_indices[d]] = self.X[i, self.dimension_indices[d]]
+        return particle_vector
+
+    def b_y(self, j, i):
+        particle_vector = self.Y_swarm.copy()
+        for d in range(j * self.s, (j + 1) * self.s):
+            particle_vector[self.dimension_indices[d]] = self.Y[i, self.dimension_indices[d]]
+        return particle_vector
+
+    # def local_best(self, j, i):
+    #     # v_i = self.fun(self.b(j, i, 'y'))
+    #     v_i = self.fY[i, j]
+    #     v_im1 = float('inf')
+    #     v_ip1 = float('inf')
+    #     if i != 0:
+    #         # v_im1 = self.fun(self.b(j, i - 1, 'y'))
+    #         v_im1 = self.fY[i - 1, j]
+    #     if i != self.population_size - 1:
+    #         # v_ip1 = self.fun(self.b(j, i + 1, 'y'))
+    #         v_ip1 = self.fY[i + 1, j]
+    #
+    #     if v_i < v_ip1 and v_i < v_im1:
+    #         return i
+    #     elif v_ip1 < v_im1:
+    #         return i + 1
+    #     else:
+    #         return i - 1
+
     def local_best(self, j, i):
-        v_i = self.fun(self.b(j, i, 'y'))
+        # v_i = self.fun(self.b(j, i, 'y'))
+        v_i = self.fY[i, j]
         v_im1 = float('inf')
         v_ip1 = float('inf')
         if i != 0:
-            v_im1 = self.fun(self.b(j, i - 1, 'y'))
+            # v_im1 = self.fun(self.b(j, i - 1, 'y'))
+            v_im1 = self.fY[i - 1, j]
+        if i == 0:
+            v_im1 = self.fY[self.population_size - 1, j]
         if i != self.population_size - 1:
-            v_ip1 = self.fun(self.b(j, i + 1, 'y'))
+            # v_ip1 = self.fun(self.b(j, i + 1, 'y'))
+            v_ip1 = self.fY[i + 1, j]
+        if i == self.population_size - 1:
+            v_ip1 = self.fY[0, j]
 
         if v_i < v_ip1 and v_i < v_im1:
             return i
         elif v_ip1 < v_im1:
+            if i + 1 >= self.population_size:
+                return 0
             return i + 1
         else:
             return i - 1
 
     def evolve(self):
-        for each_run in tqdm(range(self.iterations)):
+        each_run = 0
+        while 1:
+            each_run += 1
             # repeat
             if not self.global_improved:
                 self.s = random.choice(self.group_size_set)
@@ -91,23 +154,42 @@ class CCPSO2(object):
 
             random.shuffle(self.dimension_indices)
 
+            # 重置适应值存储
+            self.fX = np.ones([self.population_size, self.k]) * np.inf
+            self.fY = self.fX.copy()
+
             self.global_improved = False
+
+            # # 适应值计算
+            # for j in range(self.k):
+            #     for i in range(self.population_size):
+            #         self.fX[i, j] = self.fun(self.b_x(j, i))
 
             for j in range(self.k):
                 for i in range(self.population_size):
-                    if self.fun(self.b(j, i, 'x')) < self.fun(self.b(j, i, 'y')):
+                    # 适应值计算 (每个子种群的每个粒子）
+                    self.fX[i, j] = self.fun(self.b_x(j, i))
+                    self.fY[i, j] = self.fun(self.b_y(j, i))
+                    self.number_of_fitness_evaluations += 2
+                    self.pbar.update(n=2)
+                    if self.fX[i, j] < self.fY[i, j]:
                         for d in range(j * self.s, (j + 1) * self.s):
                             self.Y[i, self.dimension_indices[d]] = self.X[i, self.dimension_indices[d]]
-                    if self.fun(self.b(j, i, 'y')) < self.fun(self.b(j, i, 'swarm_best')):
+                        self.fY[i, j] = self.fX[i, j].copy()
+                    if self.fY[i, j] < self.fY_swarm:
                         for d in range(j * self.s, (j + 1) * self.s):
                             self.Y_swarm[self.dimension_indices[d]] = self.Y[i, self.dimension_indices[d]]
+                        self.fY_swarm = self.fY[i, j].copy()
                 for i in range(self.population_size):
                     local_i = self.local_best(j, i)
                     for d in range(j * self.s, (j + 1) * self.s):
                         self.Y_local[i, self.dimension_indices[d]] = self.Y[local_i, self.dimension_indices[d]]
-                if self.fun(self.b(j, 0, 'swarm_best')) < self.fun(self.Y_global):
+
+                # 这里的Y_global已经跟Y_swarm一样了
+                if self.fY_swarm < self.fY_global:
                     for d in range(j * self.s, (j + 1) * self.s):
                         self.Y_global[self.dimension_indices[d]] = self.Y_swarm[self.dimension_indices[d]]
+                    self.fY_global = self.fY_swarm
                     self.global_improved = True
 
             for j in range(self.k):
@@ -120,16 +202,27 @@ class CCPSO2(object):
                         else:
                             self.X[i, d_update] = self.Y_local[i, d_update] + norm.rvs(loc=0, scale=1, size=1) \
                                                   * abs(self.Y[i, d_update] - self.Y_local[i, d_update])
-            self.Y_global_history.append(self.fun(self.Y_global))
+                        # self.X[i, d_update] = np.clip(self.X[i, d_update], self.x_lower, self.x_upper)
+            # self.Y_global_history.append(self.fun(self.Y_global))
+            self.Y_global_history.append(self.fY_global)
 
+            # 如果适应度评估次数达到了上限(5,000,000)
+            s_min = min(self.group_size_set)
+            k_max = int(self.dimension_size / s_min)
+            if self.number_of_fitness_evaluations + (
+                    k_max * self.population_size * 2) > self.max_number_of_fitness_evaluations:
+                self.end = each_run + 1
+                break
+
+        self.pbar.close()
+        print('number_of_fitness_evaluations = ' + str(self.number_of_fitness_evaluations))
         # Plotting
-        t = np.arange(0, self.iterations, 1)
+        t = np.arange(0, self.end-1, 1)
 
         fig, ax = plt.subplots()
         ax.plot(t, self.Y_global_history)
-        ax.set(xlabel='iteration', ylabel='global best',
-               title='Evolutionary History')
+        ax.set(xlabel='iteration', ylabel='global best', title='Evolutionary History')
         ax.grid()
         plt.show()
 
-        return [self.Y_global, self.fun(self.Y_global)]
+        return self.Y_global_history, self.Y_global, self.fY_global
--- cc_test.py
+++ cc_test.py
@@ -2,16 +2,38 @@ import random
 import numpy as np
 from fitnessfunction import function1, function2
 from CCPSO2 import CCPSO2
+from cec2013lsgo.cec2013 import Benchmark
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from fitnessfunction import function3
 from tqdm import tqdm
+import pandas as pd
 
-# test (use function2)
-print("GO...")
-ccpso2 = CCPSO2(function2, 20, 1000)
-result = ccpso2.evolve()
 
-print()
+def main(fun_id):
+    bench = Benchmark()
+
+    i_dimension_size = 1000                         # 1000
+    i_population_size = 30                          # 30
+    max_number_of_fitness_evaluations = 5000000     # 5,000,000
+    ifun = bench.get_function(fun_id)
+    i_x_lower = bench.get_info(fun_id)['lower']
+    i_x_upper = bench.get_info(fun_id)['upper']
+
+    print("\nFunction Info: " + str(bench.get_info(fun_id)))
+    print('<(￣︶￣)↗[GO!]')
+
+    ccpso2 = CCPSO2(fun=ifun, dimension_size=i_dimension_size, population_size=i_population_size,
+                    max_number_of_fitness_evaluations=max_number_of_fitness_evaluations, x_lower=i_x_lower,
+                    x_upper=i_x_upper)
+    gbest_history, X, gbest = ccpso2.evolve()
+
+    pd.DataFrame(data=gbest_history).to_csv('results/CCPSO2_gbest_history_fun' + str(fun_id) + '.csv')
+    pd.DataFrame(data=X).to_csv('results/X_result_fun' + str(fun_id) + '.csv')
+    print(str(gbest))
+
+
+if __name__ == '__main__':
+    main(fun_id=1)
